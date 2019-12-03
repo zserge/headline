19d3598b49daef14968296917b07221879b1cb78
Sergii Zaitsev <sergii.zaitsev@sixt.com>
2019-12-03 14:24:46 +0100

    remove debug

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ import {Feeds} from './rss.js';
 
 const feeds = new Feeds();
 
-console.log('app loaded');
-
 let listeners = [];
 let isLoading = false;
 const useFeeds = () => {
@@ -153,11 +151,8 @@ const App = () => {
     setURLFilter(feedURL);
     setSidebarShown(false);
   };
-  console.log('app rendering');
   useEffect(() => {
-    console.log('before hash changed');
     window.location.hash = '#' + urlFilter;
-    console.log('after hash changed');
     document.title = `Headline - ${urlFilter || 'minimalist news reader'}`
   }, [urlFilter]);
   useEffect(async () => {
